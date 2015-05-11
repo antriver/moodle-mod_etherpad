fd7db38cf97bf9012b56f7672a0bb045db1cf8d9
Anthony Kuske <anthonykuske@gmail.com>
2015-05-11 22:14:09 +0200

    Add addinstance capability

--- db/access.php
+++ db/access.php
@@ -1,6 +1,19 @@
 <?php
 
 $capabilities = array(
+
+    'mod/etherpad:addinstance' => array(
+        'riskbitmask' => RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+
     'mod/etherpad:teacher' => array(
         'riskbitmask' => RISK_PERSONAL,
         'captype' => 'write',
@@ -27,6 +40,4 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     )
-)
-
-?>
+);
