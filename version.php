382d8e998cd871f9abf4e5d456c5261f2b9734f0
Anthony Kuske <anthonykuske@gmail.com>
2016-01-06 16:45:53 +0000

    Update version for moodle 30

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?PHP // $Id: version.php,v 1.0 2012/03/28 12:30:00 Serafim Panov Exp $
 
-$module->version   = 2012062801;  // The current module version (Date: YYYYMMDDXX)
-$module->requires  = 2011120500;
-$module->release = 'Version 2.3';
-$module->component = 'mod_etherpad';
-$module->maturity = MATURITY_STABLE;
-$module->cron      = 0;           // Period for cron to check this module (secs)
+$plugin->version   = 2012062801;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires  = 2011120500;
+$plugin->release = 'Version 2.3';
+$plugin->component = 'mod_etherpad';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->cron      = 0;           // Period for cron to check this module (secs)
